534b72b76a4b4629307a196486468b1bb968b791
AhmadMizan038 <ahmdmzn01@gmail.com>
2025-06-09 10:44:11 +0700

    blok ini akan di eksekusi

---
+++ ioFilewithpath.cpp
@@ -0,0 +1,55 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string baris;
+    string namaFile;
+
+    cout << "Masukkan nama file: ";
+    cin >> namaFile;
+
+    //operasi file dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file untuk membuka 
+    outfile.open(namaFile + ".txt");
+
+    cout << ">= Menulis file, \'q\'untuk keluar" << endl;
+
+    //unlimeted loop untuk menulis
+    while (true) {
+        cout << "- ";
+    //mendapatkan setiap karakter dalam satu baris
+    getline(cin,baris);
+    //loop akan berehanti jika anda memasukan karaktter q
+    if (baris == "q") break;
+    //menulis dan memasukan nilai dari baris ke dalam file
+    outfile << baris << endl;
+    }
+    // selesai dalam menulis sekarang tutup file nya
+    outfile.close();
+
+    //operasi file dalam mode membaca
+    ifstream infile;
+    //menunjuk ke sebuah file untuk membaca
+    infile.open(namaFile + ".txt");
+
+    cout << endl << ">= Memebuka dan membaca file " << endl;
+    // jika file ada maka 
+    if (infile.is_open())
+    {
+        //melakukan perulangan setiap baris 
+        while (getline(infile, baris))
+        {
+            //dan tampilkan di sini 
+            cout << baris << '\n';
+        }
+        //tutup file tersebut setelah selesai
+        infile.close();
+    }
+    // jika tidak di temukan file maka akan menampilkan ini 
+    else cout << "unable to open file";
+    return 0;
+}
--- manualExpeption.cpp
+++ manualExpeption.cpp
@@ -1,2 +1,15 @@
 #include <iostream>
 using namespace std;
+
+int main()
+{
+    try {
+        cout << "selamat belajar di prodi TI UMY" << endl;
+        throw 0,5; //melemparkan sebuah integer maka
+        cout << "Pertanyaan tidak akan di eksekusi" << endl;
+    }
+    catch (int a) {
+        //blok ini akan di eksekusi
+        cout << "Pengecaulian akan di eksekusi" << endl;
+    }
+}
